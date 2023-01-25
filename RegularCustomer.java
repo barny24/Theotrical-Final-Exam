3112f93982133985b9785640f866987446d5e736
barny24 <122979584+barny24@users.noreply.github.com>
2023-01-25 08:35:52 -0500

    Create RegularCustomer.java

---
+++ RegularCustomer.java
@@ -0,0 +1,12 @@
+public class RegularCustomer extends Customer 
+ { 
+          
+         double calculateBill(){ 
+                 return amount; 
+         } 
+          
+         RegularCustomer(String name, double amount){ 
+                 super(name, amount); 
+         } 
+ 
+ }
